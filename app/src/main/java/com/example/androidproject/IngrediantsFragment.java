8080d5987966d1be67d0764b6b4b794983b1c6d1
sammar <sammaralaa95@gmail.com>
2024-08-22 12:43:53 +0300

    edit on meal details layout

---
+++ app/src/main/java/com/example/androidproject/IngrediantsFragment.java
@@ -0,0 +1,34 @@
+package com.example.androidproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class IngrediantsFragment extends Fragment {
+
+
+    public IngrediantsFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ingrediants, container, false);
+    }
+
+}
