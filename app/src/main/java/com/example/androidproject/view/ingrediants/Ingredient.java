ce3e4bd87801183f126e69dcbe2f9e507199538f
sammar <sammaralaa95@gmail.com>
2024-08-22 12:45:50 +0300

    add ingredient and ingredientList classes

---
+++ app/src/main/java/com/example/androidproject/view/ingrediants/Ingredient.java
@@ -0,0 +1,34 @@
+package com.example.androidproject.view.ingrediants;
+
+public class Ingredient {
+
+    String ingredient;
+    String measure;
+    String imageUrl;
+
+    public Ingredient(){
+    }
+
+    public Ingredient(String ingredient,String measure){
+        this.ingredient = ingredient;
+        this.measure = measure;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/androidproject/view/ingrediants/IngredientList.java
@@ -0,0 +1,4 @@
+package com.example.androidproject.view.ingrediants;
+
+public class IngredientList {
+}
